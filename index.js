43d0d042adb279ee750671abf36169256ef004f1
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 19:56:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function BoardMember(name, homeState, training){
     this.doCharity = function () {
         return  `I like to help people.`
     }
+    this.releasePressStatement = function () {
+        return `You will see great things from Scuber.`
+    }
 }
